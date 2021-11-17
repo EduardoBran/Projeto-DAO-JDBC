0f14d7ab38397bef4579c5ad0d3cd26a7e480bc8
Eduardo Brandao <eduardo.ads1814@gmail.com>
2021-11-17 17:13:29 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Seller;
+
+public interface SellerDao {
+	
+	void insert(Seller obj); //responsável por inserir no BD este obj que eu enviar como parametro de entrada
+	void update(Seller obj);
+	void deleteById(Integer id);
+	
+	Seller findById(Integer id); //responsavel por pegar essa Id e consultar no banco de dados um obj com esse Id
+	List<Seller> findAll();	
+}
